679beff81c2249eb955778d47e1fe0402cca13fd
Mazhar Ahmed <mazhar266@gmail.com>
2018-03-13 03:32:51 +0600

    fixed the link test cases

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import allReducers from './reducers';
+
+const store = createStore(allReducers);
+
 import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
  
@@ -14,19 +24,21 @@ it('renders without crashing', () => {
 });
 
 it('links are working', () => {
-  const enzymeWrapper = mount(<App />)
+  const enzymeWrapper = mount(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  );
   // check the welcome page
   expect(enzymeWrapper.find('h2').text()).toBe('Welcome to');
 
   // check the simple interest link
-  enzymeWrapper.find('a.simple-link').simulate('click');
-  setTimeout(() => {
-    expect(enzymeWrapper.find('h2').text()).toBe('Simple Interest');
-  }, 500);
+  enzymeWrapper.find('a.simple-link').simulate('click', { button: 0 });
+  expect(enzymeWrapper.find('h2').text()).toBe('Simple Interest');
 
   // check the compound interest link
-  enzymeWrapper.find('a.compound-link').simulate('click');
-  setTimeout(() => {
-    expect(enzymeWrapper.find('h2').text()).toBe('Compound Interest');
-  }, 500);
+  enzymeWrapper.find('a.compound-link').simulate('click', { button: 0 });
+  expect(enzymeWrapper.find('h2').text()).toBe('Compound Interest');
 });
